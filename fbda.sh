0f346c454fc8bb015ce8580214850a08eb968074
Foggalong <joshua.h.fogg@gmail.com>
2014-03-03 17:05:15 +0000

    Update fbda.sh
    Added the relevant lines for inclusion of the simple date graph.

--- fbda.sh
+++ fbda.sh
@@ -13,12 +13,13 @@ python3 scrape.py
 
 # Running analysis with R files
 echo Running analysis...
+R -q --no-save < date_graph.r
 R -q --no-save < time_graph.r
 R -q --no-save < event_pie.r
 
 # Compiling results
 echo Creating report document...
-pdftk time_graph.pdf event_pie.pdf cat output report.pdf
+pdftk date_graph.pdf time_graph.pdf event_pie.pdf cat output report.pdf
 
 # Tidying Up the files
 echo Tidying Up...
